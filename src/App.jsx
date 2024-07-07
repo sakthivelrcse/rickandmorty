cebde477621ec1db9b07b3c3b896f62936b049d4
svishere <sakthivel100gym@gmail.com>
2024-07-07 20:12:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CharacterList from './components/CharacterList'
+
+const App = () => {
+  return (
+    <div>
+      <CharacterList/>
+    </div>
+  )
+}
+
+export default App
